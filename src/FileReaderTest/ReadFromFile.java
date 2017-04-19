a1a12f23dc9eb5a92295703a7eaee6cce7748869
Sambit <Sambit@192.168.1.4>
2017-04-19 16:59:35 +0530

    No changes

---
+++ src/FileReaderTest/ReadFromFile.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileReaderTest;
+//import java.io.BufferedReader;
+import java.io.*;
+
+/**
+ *
+ * @author Sambit
+ */
+public class ReadFromFile {
+    
+    public ReadFromFile(String myFile)                                  // constructor
+            throws FileNotFoundException, IOException{
+                //BufferedReader br;
+                
+                BufferedReader br = new BufferedReader(new FileReader(myFile));        // Reads from a specified File 
+                String line = "";
+                
+                while( (line = br.readLine()) !=null){
+                    System.out.println("Reading line : "+line);
+                }
+                
+    }
+    
+    public static void main(String args[]) 
+            throws FileNotFoundException, IOException{
+        
+                new ReadFromFile("D:\\Sambit\\NetBeansProjects\\JavaApplicationProject1\\myTestFile2.txt");         // when the class is instantiated, it invokes the constructor
+       
+    }
+            
+    
+}
---
+++ src/serializationTest/DePersist.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializationTest;
+import java.io.*;
+
+/**
+ *
+ * @author Sambit
+ */
+public class DePersist {
+    
+    public static void main(String args[]) throws Exception{
+        
+        /* mystudent object will be retrieved(de-prsisted) from the byte stream read from the file "file1.txt" 
+            using readObject() method */
+        
+        //FileInputStream = new FileInputStream("file1.txt");
+        ObjectInputStream objIn = new ObjectInputStream(new FileInputStream("D:/Sambit/NetBeansProjects/JavaApplicationProject1/myTestFile.txt"));
+        Student mystudent = (Student)objIn.readObject();
+        
+        System.out.println("Student ID="+mystudent.id);
+        System.out.println("Student ID="+mystudent.name.toString());
+        
+        
+        
+    }
+    
+}
