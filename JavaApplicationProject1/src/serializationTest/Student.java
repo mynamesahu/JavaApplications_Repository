8fa0e06cf6910dead8d4da9e214e052fccea58e1
Sambit <Sambit@192.168.1.4>
2017-04-24 21:18:50 +0530

    copied and pasted

---
+++ JavaApplicationProject1/src/serializationTest/Student.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package serializationTest;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Sambit
+ * This is an java object which can be converted to byte stream 
+ * and its state to be persisted over a network by writing to / reading from a file 
+ * (by implementing Serializable interface)
+ */
+public class Student implements Serializable
+{
+    int id;
+    String name;
+    
+    public Student(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    
+}
