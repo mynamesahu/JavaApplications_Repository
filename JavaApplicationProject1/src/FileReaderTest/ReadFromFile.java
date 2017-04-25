e7f0d4e936bda5ffad3c9712912c48044632ab7c
Sambit <Sambit@192.168.1.4>
2017-04-25 13:46:16 +0530

    changed the path of  "myFile2.txt" to the appropriate location

--- JavaApplicationProject1/src/FileReaderTest/ReadFromFile.java
+++ JavaApplicationProject1/src/FileReaderTest/ReadFromFile.java
@@ -13,11 +13,11 @@ import java.io.*;
  */
 public class ReadFromFile {
     
-    public ReadFromFile(String myFile)                                  // constructor
+    public ReadFromFile(String myFile)                                              // constructor
             throws FileNotFoundException, IOException{
                 //BufferedReader br;
                 
-                BufferedReader br = new BufferedReader(new FileReader(myFile));        // Reads from a specified File 
+                BufferedReader br = new BufferedReader(new FileReader(myFile));    // Reads from a specified File 
                 String line = "";
                 
                 while( (line = br.readLine()) !=null){
@@ -29,7 +29,7 @@ public class ReadFromFile {
     public static void main(String args[]) 
             throws FileNotFoundException, IOException{
         
-                new ReadFromFile("D:\\Sambit\\NetBeansProjects\\JavaApplicationProject1\\myTestFile2.txt");         // when the class is instantiated, it invokes the constructor
+                new ReadFromFile("D:\\Sambit\\NetBeansProjects\\JavaApplications_Repository\\JavaApplicationProject1\\myTestFile2.txt");         // when the class is instantiated, it invokes the constructor
        
     }
             
